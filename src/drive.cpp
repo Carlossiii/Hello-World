92f16e5be6b88eecf889e9189543667c9e463eed
carlossiii <cv5673@gmail.com>
2016-06-18 02:49:34 -0300

    Adição de entrada pelo usuário

--- src/drive.cpp
+++ src/drive.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 
-int main (){
-	std::cout << "Hello World" << std::endl;
+using namespace std;
+
+int main (int argc, char * argv[]){
+	std::string input;
+	if (argc == 2){
+		input = (std::string)argv[1];
+		std::cout << input << std::endl;
+	}
+	else{
+		std::cout << "Hello World" << std::endl;
+	}
 	return 0;
 }
